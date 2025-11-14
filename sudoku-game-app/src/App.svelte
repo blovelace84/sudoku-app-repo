95b7dbf0d45c1836dbb45b554cc8f65ddd950374
Brittani Lovelace <blovelace84@gmail.com>
2025-11-14 17:12:32 -0500

    centering my game board

--- sudoku-game-app/src/App.svelte
+++ sudoku-game-app/src/App.svelte
@@ -8,8 +8,28 @@
   editable.set(generateEditableMask(initialPuzzle));
 </script>
 
-<main>
+<main class="app">
   <h1>Sudoku App</h1>
-  <SudokuGrid />
-  <Controls />
+  <div class="grid-wrapper">
+    <SudokuGrid />
+    <Controls />
+  </div>
 </main>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    background-color: lightblue;
+    min-height: 100vh;
+  }
+
+  .grid-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
